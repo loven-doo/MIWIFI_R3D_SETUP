57e2e4f45978b893e8656d3de1ba6afdd1c1ab5c
loven-doo <1429305+loven-doo@users.noreply.github.com>
2020-01-04 20:55:24 +0300

    Update data_mounts.sh

--- data_mounts.sh
+++ data_mounts.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/sh /etc/rc.common
 
-boot() {
-       export TERM=xterm
+start() {
+    export TERM=xterm
 
-       mount --bind /tmp /data/tmp
-       mount --bind /dev /data/dev
-       mount --bind /proc /data/proc
-       mount --bind /etc /data/etc
-       mount --bind /sys /data/sys
-       mount --bind /var /data/var
+    mount --bind /tmp /data/tmp
+    mount --bind /dev /data/dev
+    mount --bind /proc /data/proc
+    mount --bind /etc /data/etc
+    mount --bind /sys /data/sys
+    mount --bind /var /data/var
 }
