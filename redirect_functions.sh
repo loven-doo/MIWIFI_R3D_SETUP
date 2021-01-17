0d973927edc0c7ec3e23f14f44d92a1546d4dac7
loven-doo <1429305+loven-doo@users.noreply.github.com>
2021-01-17 11:50:24 +0300

    redirect_functions.sh first version

--- redirect_functions.sh
+++ redirect_functions.sh
@@ -3,5 +3,12 @@
 TO_REPLACE=/lib/functions.sh
 REPLACE_WITH=/data/lib/functions.sh
 
-ls /data/usr/lib/opkg/info/*.postinst
-ls /data/usr/lib/opkg/info/*.prerm
+for FPATH in $(ls -d /data/usr/lib/opkg/info/*.postinst)
+do
+    sed -i s#$TO_REPLACE#$REPLACE_WITH#g $FPATH
+done
+
+for FPATH in $(ls -d /data/usr/lib/opkg/info/*.prerm)
+do
+    sed -i s#$TO_REPLACE#$REPLACE_WITH#g $FPATH
+done
