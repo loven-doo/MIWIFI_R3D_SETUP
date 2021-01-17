439f0ada22bd2cc872006e6081c6e125cf79fa37
loven-doo <1429305+loven-doo@users.noreply.github.com>
2021-01-17 11:35:27 +0300

    Update redirect_functions.sh

--- redirect_functions.sh
+++ redirect_functions.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-# not written yet, will be soon
+
+TO_REPLACE=/lib/functions.sh
+REPLACE_WITH=/data/lib/functions.sh
+
+ls /data/usr/lib/opkg/info/*.postinst
+ls /data/usr/lib/opkg/info/*.prerm
